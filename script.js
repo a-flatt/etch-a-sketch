4827548e5b9335c2e1e5add236a1db609aa42015
Adam Platt <adamjplatt@outlook.com>
2024-05-15 17:15:00 +0800

    flex working

--- script.js
+++ script.js
@@ -1,18 +1,28 @@
 const container = document.querySelector(".container");
+const button = document.querySelector("button");
 
-for (i = 0; i < 16; i++){
+function paintGrid (gridSize) {
+    
+    container.replaceChildren();
+    
+    for (i = 0; i < gridSize; i++){
 
-    const row = document.createElement("div")
-    row.setAttribute("class", "row-normal");
-    container.appendChild(row);
-
-    for (j = 0; j < 16; j++) {
-        const column = document.createElement("div")
-        column.setAttribute("class", "column-normal");
-        row.appendChild(column);
-        column.addEventListener("mouseover", () => {
-            column.setAttribute("class", "column-hover");
-        });
-    }
+        const row = document.createElement("div")
+        row.setAttribute("class", "row-normal");
+        container.appendChild(row);
+    
+        for (j = 0; j < gridSize; j++) {
+            const column = document.createElement("div")
+            column.setAttribute("class", "column-normal");
+            row.appendChild(column);
+            column.addEventListener("mouseover", () => {
+                column.setAttribute("class", "column-hover");
+            });
+        }
+    }    
 }
 
+button.addEventListener("click", () => {
+    let gridSize = prompt("Please enter the new grid size.");
+    paintGrid(gridSize);
+});
