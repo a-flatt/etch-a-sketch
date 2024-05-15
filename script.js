7de0473bf215f212bd8481b209eaaf08ae6fa6fb
Adam Platt <adamjplatt@outlook.com>
2024-05-15 16:52:33 +0800

    painting grid

---
+++ script.js
@@ -0,0 +1,14 @@
+const container = document.querySelector(".container");
+
+for (i = 0; i < 16; i++){
+
+    const row = document.createElement("div")
+    row.setAttribute("class", "row-normal");
+    container.appendChild(row);
+
+    for (j = 0; j < 16; j++) {
+        const column = document.createElement("div")
+        column.setAttribute("class", "column-normal");
+        row.appendChild(column);
+    }
+}
